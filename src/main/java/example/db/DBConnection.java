9e4c4a1b99ef5e1c23146fa487be87186ce0aec3
quynhvi <quynh.nguyen@mservice.com.vn>
2020-02-13 23:39:23 +0700

    establish a jdbc database connection

--- src/main/java/example/db/DBConnection.java
+++ src/main/java/example/db/DBConnection.java
@@ -1,17 +1,10 @@
-package example;
+package example.db;
 
-import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Future;
 import io.vertx.core.Promise;
 
-public class MainVerticle extends AbstractVerticle {
-    @Override
-    public void start(Promise<Void> promise) throws Exception {
-        Future<Void> steps = prepareDatabase().compose(v -> startHttpServer());
-        steps.setHandler(promise);
-        promise.complete();
-    }
+import java.util.concurrent.Future;
 
+public class DBConnection {
     private Future<Void> prepareDatabase() {
         Promise<void> promise = Promise.promise();
         // (...)
