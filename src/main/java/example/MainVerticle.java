e19c37604efd6630a2b3758934f5a802e757de6f
quynhvi <quynh.nguyen@mservice.com.vn>
2020-02-17 23:51:39 +0700

    implement of the prepareDatabase method

--- src/main/java/example/MainVerticle.java
+++ src/main/java/example/MainVerticle.java
@@ -3,8 +3,18 @@ package example;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
 import io.vertx.core.Promise;
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import io.vertx.ext.jdbc.JDBCClient;
+import io.vertx.ext.sql.SQLConnection;
+
+import static example.db.DBConFig.SQL_CREATE_PAGES_TABLE;
 
 public class MainVerticle extends AbstractVerticle {
+
+    private JDBCClient dbClient;
+    private static final Logger LOGGER = LoggerFactory.getLogger(MainVerticle.class);
     @Override
     public void start(Promise<Void> promise) throws Exception {
         Future<Void> steps = prepareDatabase().compose(v -> startHttpServer());
@@ -18,9 +28,32 @@ public class MainVerticle extends AbstractVerticle {
     }
 
     private Future<Void> prepareDatabase() {
-        Promise<void> promise = Promise.promise();
-        // (...)
-        return (Future<Void>) promise.future();
+        Promise<Void> promise = Promise.promise();
+
+        dbClient = JDBCClient.createShared(vertx, new JsonObject()
+                .put("url", "jdbc:hsqldb:file:db/wiki")
+                .put("driver_class", "org.hsqldb.jdbcDriver")
+                .put("max_pool_size", 30));
+
+        dbClient.getConnection(ar -> {
+            if (ar.failed()) {
+                LOGGER.error("Could not open a database connection", ar.cause());
+                promise.fail(ar.cause());
+            } else {
+                SQLConnection connection = ar.result();
+                connection.execute(SQL_CREATE_PAGES_TABLE, create -> {
+                    connection.close();
+                    if (create.failed()) {
+                        LOGGER.error("Database preparation error", create.cause());
+                        promise.fail(create.cause());
+                    } else {
+                        promise.complete();
+                    }
+                });
+            }
+        });
+
+        return promise.future();
     }
 
     private Future<Void> startHttpServer() {
--- src/main/java/example/db/DBConnection.java
+++ src/main/java/example/db/DBConnection.java
@@ -5,15 +5,15 @@ import io.vertx.core.Promise;
 import java.util.concurrent.Future;
 
 public class DBConnection {
-    private Future<Void> prepareDatabase() {
-        Promise<void> promise = Promise.promise();
-        // (...)
-        return (Future<Void>) promise.future();
-    }
-
-    private Future<Void> startHttpServer() {
-        Promise<void> promise = Promise.promise();
-        // (...)
-        return (Future<Void>) promise.future();
-    }
+//    private Future<Void> prepareDatabase() {
+//        Promise<void> promise = Promise.promise();
+//        // (...)
+//        return (Future<Void>) promise.future();
+//    }
+//
+//    private Future<Void> startHttpServer() {
+//        Promise<void> promise = Promise.promise();
+//        // (...)
+//        return (Future<Void>) promise.future();
+//    }
 }
