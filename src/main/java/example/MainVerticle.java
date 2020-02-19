681ab032ee3a6dd2fc42ca991e1986a1a95e8b40
quynhvi <quynh.nguyen@mservice.com.vn>
2020-02-19 23:23:48 +0700

    init branch future objects and callbacks

---
+++ src/main/java/example/MainVerticle.java
@@ -0,0 +1,11 @@
+package example;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+
+public class MainVerticle extends AbstractVerticle {
+    @Override
+    public void start(Promise<Void> promise) {
+        promise.complete();
+    }
+}
